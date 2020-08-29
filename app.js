a6a7d662a3c8c6ad3d8eb6a9239695ce15e6d498
phatdroid94 <phatdroid94@gmail.com>
2020-08-29 19:35:33 +0700

    bug

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var db = require('./db');
+var AuthController = require('./user/AuthController');
+
+var UserController = require('./user/UserController');
+app.use('/api/auth', AuthController);
+app.use('/users', UserController);
+
+module.exports = app;
---
+++ db.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://abc:Wj3b99tVr69EhQBH@cluster0.jdeci.mongodb.net/DatabaseUser?retryWrites=true&w=majority');
+
+
+
